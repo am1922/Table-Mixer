4a0e0126d6faee6727ac81e3bc2992260aaf218a
Artem Mikhaylov <m.av1922@yandex.ru>
2023-07-12 20:22:32 +0300

    Initial Commit

---
+++ Table-Mixer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table-Mixer
+//
+//  Created by Артем Михайлов on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
